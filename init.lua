cd384b9ee482a744354391957e72298a3f38ae81
peter willumsen <pewillumsen@gmail.com>
2024-04-23 12:25:14 +0200

    enabled nerd fonts

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -881,6 +881,7 @@ require('lazy').setup({
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    --
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
