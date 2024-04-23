ebbe393319cee92a0a3dbd9fcd4d785289c94aa4
peter willumsen <pewillumsen@gmail.com>
2024-04-23 12:32:14 +0200

    added arrow

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,4 +17,12 @@ return {
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
+  {
+    'otavioschwanck/arrow.nvim',
+    opts = {
+      show_icons = true,
+      leader_key = ';', -- Recommended to be a single key
+      buffer_leader_key = 'm', -- Per Buffer Mappings
+    },
+  },
 }
