779ece19f9f7063dd9939c695bf3743e4b917e58
peter willumsen <pewillumsen@gmail.com>
2024-07-17 00:34:31 +0200

    changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -25,22 +25,22 @@ return {
       leader_key = ';', -- Recommended to be a single key
       buffer_leader_key = 'm', -- Per Buffer Mappings
     },
-    {
-      'christoomey/vim-tmux-navigator',
-      cmd = {
-        'TmuxNavigateLeft',
-        'TmuxNavigateDown',
-        'TmuxNavigateUp',
-        'TmuxNavigateRight',
-        'TmuxNavigatePrevious',
-      },
-      keys = {
-        { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-        { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-        { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-        { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-        { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-      },
+  },
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
   -- {
