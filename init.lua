f7db2a6b5b244a298f2ff814e170c7a39aebad3d
Peter Willumsen <pewillumsen@gmail.com>
2024-12-11 14:10:19 +0100

    update

--- init.lua
+++ init.lua
@@ -732,7 +732,7 @@ require('lazy').setup({
     cmd = { 'ConformInfo' },
     keys = {
       {
-        '<leader>f',
+        '<leader>cf',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
         end,
